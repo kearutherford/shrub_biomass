2e4fa44f4fc79a0b390f28ac76142434eb9d4553
kearutherford <krutherford@berkeley.edu>
2025-03-03 09:07:29 -0800

    save BFA shrub script

--- shrub_bio.R
+++ shrub_bio.R
@@ -40,7 +40,7 @@ ShrubBiomass <- function(data, results = "by_plot") {
   step0 <- as.data.frame(data)
 
   # check input data
-  # ValidateShrubs(data = step0)
+  ValidateShrubs(data = step0, results_val = results)
 
   # calculate shrub-level biomass
   step1 <- PredictShrubBio(step0)
@@ -66,12 +66,245 @@ ShrubBiomass <- function(data, results = "by_plot") {
 ################################################################################
 ################################################################################
 
-ValidateShrubs <- function(data) {
+ValidateShrubs <- function(data, results_val) {
 
+  ###########################################################
+  # check that results options are set appropriately
+  ###########################################################
 
+  if(results_val == "by_shrub" || results_val == "by_plot") {
+    # do nothing
+  } else {
+    stop('The "results" parameter must be set to either "by_shrub" or "by_plot".')
+  }
 
+  ###########################################################
+  # Check that all columns are in the provided dataframe
+  ###########################################################
 
+  if(!("site" %in% colnames(data))) {
+    stop('Input data is missing the necessary "site" column.')
+  }
 
+  if(!("plot" %in% colnames(data))) {
+    stop('Input data is missing the necessary "plot" column.')
+  }
+
+  if(!("transect" %in% colnames(data))) {
+    stop('Input data is missing the necessary "transect" column.')
+  }
+
+  if(!("tran_length" %in% colnames(data))) {
+    stop('Input data is missing the necessary "tran_length" column.')
+  }
+
+  if(!("species" %in% colnames(data))) {
+    stop('Input data is missing the necessary "species" column.')
+  }
+
+  if(!("ht" %in% colnames(data))) {
+    stop('Input data is missing the necessary "ht" column.')
+  }
+
+  if(!("crown_axis_1" %in% colnames(data))) {
+    stop('Input data is missing the necessary "crown_axis_1" column.')
+  }
+
+  if(!("crown_axis_2" %in% colnames(data))) {
+    stop('Input data is missing the necessary "crown_axis_2" column.')
+  }
+
+  ###########################################################
+  # Check that column classes are as expected
+  ###########################################################
+
+  # Categorical variables ------------------------------------------------------
+  if(!is.character(data$site)) {
+    stop('"site" must be a character variable.\n',
+         'You have input a variable of class: ', class(data$site))
+  }
+
+  if(!is.character(data$plot)) {
+    stop('"plot" must be a character variable.\n',
+         'You have input a variable of class: ', class(data$plot))
+  }
+
+  if(!is.character(data$transect)) {
+    stop('"transect" must be a character variable.\n',
+         'You have input a variable of class: ', class(data$transect))
+  }
+
+  if(!is.character(data$species)) {
+    stop('"species" must be a character variable.\n',
+         'You have input a variable of class: ', class(data$species))
+  }
+
+  # Numerical variables --------------------------------------------------------
+  if(!is.numeric(data$tran_length)) {
+    stop('"tran_length" must be a numerical variable.\n',
+         'You have input a variable of class: ', class(data$tran_length))
+  }
+
+  if(!is.numeric(data$ht)) {
+    stop('"ht" must be a numerical variable.\n',
+         'You have input a variable of class: ', class(data$ht))
+  }
+
+  if(!is.numeric(data$crown_axis_1)) {
+    stop('"crown_axis_1" must be a numerical variable.\n',
+         'You have input a variable of class: ', class(data$crown_axis_1))
+  }
+
+  if(!is.numeric(data$crown_axis_2)) {
+    stop('"crown_axis_2" must be a numerical variable.\n',
+         'You have input a variable of class: ', class(data$crown_axis_2))
+  }
+
+  #########################################################
+  # Check that site, plot, and transect are as expected
+  #########################################################
+
+  if ('TRUE' %in% is.na(data$site)) {
+    stop('There are missing values in the site column in the provided dataframe.')
+  }
+
+  if ('TRUE' %in% is.na(data$plot)) {
+    stop('There are missing values in the plot column in the provided dataframe.')
+  }
+
+  if ('TRUE' %in% is.na(data$transect)) {
+    stop('There are missing values in the transect column in the provided dataframe.')
+  }
+
+  ###########################################################
+  # Check transect length
+  ###########################################################
+
+  # Check for NA ---------------------------------------------------------------
+  if ('TRUE' %in% is.na(data$tran_length)) {
+    stop('There are missing transect lengths in the provided dataframe.')
+  }
+
+  # Check for negative values ---------------------------------------------------
+  if (min(data$tran_length, na.rm = TRUE) <= 0) {
+    stop('There are transect lengths <= 0 in the provided dataframe. All tran_length values must be > 0.')
+  }
+
+  # Check for matching values --------------------------------------------------
+  data$site_plot_tran <- paste(data$site, data$plot, data$transect, sep = "_")
+  unq_ids <- unique(data$site_plot_tran)
+
+  tl_check_vec <- c()
+
+  for(u in unq_ids) {
+
+    all_shrubs <- subset(data, site_plot_tran == u)
+
+    if(length(unique(all_shrubs$tran_length)) != 1) {
+      tl_check_vec <- c(tl_check_vec, u)
+    }
+
+  }
+
+  if(length(tl_check_vec) > 0) {
+
+    tl_check <- paste0(tl_check_vec, sep = "   ")
+
+    stop('Each site:plot:transect should have the same transect length recorded.\n',
+         'The following site:plot:transect combinations have multiple tran_length values: ', tl_check)
+
+  }
+
+  ###########################################################
+  # Check that species codes are as expected
+  ###########################################################
+
+  # Check for unrecognized species codes ---------------------------------------
+  sp_code_names <- c("AMAL", "ARPA", "ARVI", "CESP", "CECO", "CEIN", "CEVE", "CHSP", "CHSE",
+                     "COSP", "COCO", "LEDA", "NODE", "QUSP", "RISP", "OTHER", "NONE")
+
+  if(!all(is.element(data$species, sp_code_names))) {
+
+    unrecognized_sp <- sort(paste0(unique(data[!is.element(data$species, sp_code_names), "species"]), sep = " "))
+
+    stop('Not all species codes were recognized!\n',
+         'Unrecognized codes: ', unrecognized_sp)
+
+  }
+
+  # Check for NA ---------------------------------------------------------------
+  if ('TRUE' %in% is.na(data$species)) {
+    stop('There are missing species codes in the provided dataframe. Consider using OTHER or NONE for these shrubs.')
+  }
+
+  # Check for proper use of NONE species ---------------------------------------
+  for(u in unq_ids) {
+
+    all_shrubs <- subset(data, site_plot_tran == u)
+
+    if('TRUE' %in% is.element(all_shrubs$species, "NONE")) {
+
+      n <- nrow(all_shrubs)
+
+      if(n > 1) {
+
+        stop('There are transects with a recorded species code of NONE, but with more than one row.\n',
+             'Transects with no shrubs should be represented by a single row with site, plot, transect, and tran_length filled in as appropriate and a species code of NONE.')
+
+      }
+
+    }
+
+  }
+
+  trans_wo_shrubs <- subset(data, species == "NONE",
+                            select = c(ht, crown_axis_1, crown_axis_2))
+
+  if('FALSE' %in% is.na(trans_wo_shrubs)) {
+
+    warning('There are transects with a recorded species code of NONE, but with non-NA ht, crown_axis_1 and/or crown_axis_2.\n',
+            'Transects with no shrubs should be represented by a single row with site, plot, transect, and tran_length filled in as appropriate,\n',
+            'a species code of NONE, and NA ht, crown_axis_1, and/or crown_axis_2. Consider investigating these entries.')
+
+  }
+
+  ###########################################################
+  # Check height
+  ###########################################################
+
+  trans_w_shrubs <- subset(data, species != "NONE")
+
+  # Check for NA ---------------------------------------------------------------
+  if ('TRUE' %in% is.na(trans_w_shrubs$ht)) {
+    stop('There are missing shrub heights in the provided dataframe.')
+  }
+
+  # Check for negative values ---------------------------------------------------
+  if (min(trans_w_shrubs$ht) <= 0) {
+    stop('There are shrub heights <= 0 in the provided dataframe. All heights must be > 0.')
+  }
+
+  ###########################################################
+  # Check crown axis
+  ###########################################################
+
+  # Check for NA ---------------------------------------------------------------
+  if ('TRUE' %in% is.na(trans_w_shrubs$crown_axis_1)) {
+    stop('There are missing crown axis 1 values in the provided dataframe.')
+  }
+
+  if ('TRUE' %in% is.na(trans_w_shrubs$crown_axis_2)) {
+    stop('There are missing crown axis 2 values in the provided dataframe.')
+  }
+
+  # Check for negative values ---------------------------------------------------
+  if (min(trans_w_shrubs$crown_axis_1) <= 0) {
+    stop('There are crown axis 1 values <= 0 in the provided dataframe. All values must be > 0.')
+  }
+
+  if (min(trans_w_shrubs$crown_axis_2) <= 0) {
+    stop('There are crown axis 2 values <= 0 in the provided dataframe. All values must be > 0.')
+  }
 
 }
 
